4793b16181c65ee920987885c889c39a5771f11e
tracer-ysyx <tracer@ysyx.org>
2025-01-16 21:57:44 +0800

    > compile NEMU ysyx_22040000 HuoRuiZhi Tan Linux sherry-VMware-Virtual-Platform 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:57:44 up 26 min,  1 user,  load average: 0.10, 0.06, 0.15

--- nemu/src/isa/x86/include/isa-def.h
+++ nemu/src/isa/x86/include/isa-def.h
@@ -33,17 +33,19 @@ typedef struct {
 //    uint16_t _16;
 //    uint8_t _8[2];
 //  } gpr[8];
-
-    struct {
-      union{
-	     uint32_t _32;
-	     uint16_t _16;
-	     uint8_t _8[2];
-      };
-    }gpr[8];
-  /* Do NOT change the order of the GPRs' definitions. */
-  uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
+   union{
+        struct {
+          union{
+    	     uint32_t _32;
+    	     uint16_t _16;
+    	     uint8_t _8[2];
+          };
+        }gpr[8];
+      /* Do NOT change the order of the GPRs' definitions. */
+        struct{
+          uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+        };
+   };
   vaddr_t pc;
 } x86_CPU_state;
 
