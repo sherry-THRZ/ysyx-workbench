8f723a4618a7d408862cd5b3b7ece044d27c043d
tracer-ysyx <tracer@ysyx.org>
2025-01-23 15:40:46 +0800

    > compile NEMU ysyx_22040000 HuoRuiZhi Tan Linux sherry-VMware-Virtual-Platform 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:40:46 up  2:51,  1 user,  load average: 0.06, 0.35, 0.30

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -203,6 +203,15 @@ int find_mainop(int p, int q){
 	return mainop;
 }
 
+//过滤有除0的情况
+static bool check_zero(uint32_t val2){
+	if (val2 == 0){
+		panic("Error: divide zero\n");
+	}
+
+	return true;
+}
+
 uint32_t eval(int p, int q) {
   if (p > q) {
     /* Bad expression */
@@ -245,7 +254,9 @@ uint32_t eval(int p, int q) {
       case '+': return val1 + val2;
       case '-': return val1 - val2;
       case '*': return val1 * val2;
-      case '/': return val1 / val2;
+      case '/': 
+	       check_zero(val2);
+	       return val1 / val2;
       default: assert(0);
     }
   }
